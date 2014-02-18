cd4dae072de70cb258657236865b5e9fb44eeeec
Priscilla <priscilla.fialho@gmail.com>
2014-02-18 14:50:46 +0000

    Add changes in both files together

--- capi.sh
+++ capi.sh
@@ -1,1 +1,3 @@
-This is a script about what the capi questionnaire will be. 
+This is a script about what the capi questionnaire is.
+
+This is a script that could actually be a code. In any language
